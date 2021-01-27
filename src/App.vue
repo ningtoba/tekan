541b5f942a5eb13d4beb038946ff355a2cc8132d
Muhammad Luqman Hakim <jawbajaw40@gmail.com>
2021-01-27 13:42:44 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-h-screen max-h-screen bg-purple-800">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
