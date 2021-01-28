027f62c1102667dec5efa9fb9d750cd647b1b151
Muhammad Luqman Hakim <jawbajaw40@gmail.com>
2021-01-28 19:24:27 +0800

    hiatus indefinitely

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -2,7 +2,7 @@
   <div class="border-2">
     <div class="p-10 h-96 flex-col"> 
       <div class="text-center text-xl text-white flex">
-        <div v-for="word in words" :key="word.index" class="ml-1">
+        <div v-for="word in words" :key="word.index" class="ml-1 inactiveWords">
           <span v-for="letter in phrase(word)" :key="letter.index">{{letter}}</span>
         </div>
       </div>
@@ -10,7 +10,7 @@
         <span class="border-2 p-4 rounded-2xl font-bold">{{author}}</span>
       </div>
       <div>
-        <input type="text" class="w-full mt-5">
+        <input v-model="keyInput" @keypress="activeWord(keyInput)"  type="text" class="w-full mt-5">
       </div>
     </div>
   </div>
@@ -21,19 +21,21 @@ import axios from 'axios'
 export default {
   name: 'Word',
   data() {
-    return{
+    return {
       content: null,
       author: null,
       words: [],
-      alph: []
+      alph: [],
+      nextWord: String,
+      keyInput: ''
     }
   },
 
   created() {
-    axios.get('https://api.quotable.io/random').then(response => (this.content = response.data.content)).then(() => {
+    axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random').then(response => (this.content = response.data.data[0].quoteText)).then(() => {
       this.words = this.content.split(' ')
     })
-    axios.get('https://api.quotable.io/random').then(response => (this.author = response.data.author))
+    axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random').then(response => (this.author = response.data.data[0].quoteAuthor))
   },
 
   methods: {
@@ -43,8 +45,27 @@ export default {
         this.alph.push(value)
       }
       this.alph.push(' ')
+      // console.log(this.words)
+      // console.log(this.alph)
+
       return test
     },
+
+    activeWord(keyInput) {
+      if (keyInput !== '') {
+        console.log(keyInput)
+        keyInput = ''
+        console.log(keyInput)
+      }
+      // for(let value of this.words) {
+      //   if(keyInput === value[' ']) {
+      //     value++
+      //     console.log(value)
+      //   }
+      // }
+      // console.log(keyInput)
+    },
+
     keyCompare() {
 
     }
